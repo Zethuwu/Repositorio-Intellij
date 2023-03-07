bf7a327887b64158562551c21a206db9bf5a338e
Zethuwu <oscardelvillar39@aragon.unam.mx>
2023-03-06 18:18:12 -0600

    Actualizacion de codigos

--- src/Codigos2/Ejercicio1Oscar.java
+++ src/Codigos2/Ejercicio1Oscar.java
@@ -6,7 +6,6 @@ public class Ejercicio1Oscar {
         //Ejemplo heredar
         Estudiante universitario = new Estudiante("Carlos",20,176,"19/05/2002",
                 "azul","Universidad","Unam");
-
         System.out.println(universitario);
         //Ejemplo polimorfismo
         Andres andres = new Andres("Andres",19,180,"negro","25/08/2003");
@@ -21,6 +20,8 @@ public class Ejercicio1Oscar {
         InstanciaAbstrac classabs = new InstanciaAbstrac("Saul",26,175,"06/01/1997","Azul marqueza");
         System.out.println("Personalidad de " +classabs.getNombre()+": ");classabs.Personalidad();
         System.out.println(classabs.getEstatura());
+        //Ejemplo interfaz
+
 
     }
 
